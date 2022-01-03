7c1cd33b37c0361ff166d1a54fbab81212a8c5cc
alex.aspa <alex.aspa@students.salle.url.edu>
2022-01-03 22:24:12 +0100

    Second commit, error eat

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import math
 import sys
 import random
 import neat
+import time
 
 screen_width = 1500
 screen_height = 800
 generation = 0
 
 class Criatura:
-    def __init__(self,screen,clock,id):
+    def __init__(self,screen,clock,id,rad,r,g,b):
         self.id = id
-        self.radius = int(random.randint(1, 60))
+        self.radius = rad
         self.pos = [int(random.randint(1, screen_width - self.radius)), int(random.randint(1,screen_height - self.radius))]
-        self.rect = pygame.draw.circle(screen, (255,0,0), (self.pos[0], self.pos[1]), self.radius, 3)
         self.angle = 0
         self.speed = 0
         self.radars = []
@@ -23,18 +23,16 @@ class Criatura:
         self.is_alive = True
         self.time = clock
         self.hungry = 100
+        self.color = [r,g,b]
 
 
     def update(self,cars,screen):
         # check speed
-        self.speed = 4
+        self.speed = 5
 
         #move
         self.pos[0] += math.cos(math.radians(360 - self.angle)) * self.speed
         self.pos[1] += math.sin(math.radians(360 - self.angle)) * self.speed
-
-        self.rect = pygame.draw.circle(screen, (0,0,0), (self.pos[0], self.pos[1]), self.radius, 3)
-        self.rect = pygame.draw.circle(screen, (255,0,0), (self.pos[0], self.pos[1]), self.radius, 3)
 
         self.check_collision(cars)
         self.radars.clear()
@@ -47,27 +45,37 @@ class Criatura:
         self.is_alive = True
 
         for car in cars:
-            if not (self.id == car.id):
+            if (not (self.id == car.id)) and (car.get_alive()):
                 dist = math.dist([self.pos[0], self.pos[1]], [car.pos[0], car.pos[1]]) #https://stackoverflow.com/questions/22135712/pygame-collision-detection-with-two-circles
                 #dist  = math.hypot(car.pos[0] - self.pos[0], car.pos[1] - self.pos[1])
                 if dist < (self.radius + car.radius):
                     if (self.radius < car.radius):
                         self.is_alive = False
-                    else:
-                        self.hungry = self.hungry + 200
+                    elif self.radius > car.radius:
+                        print("Eat")
+                        print(self.id)
+                        self.hungry = self.hungry + 100
+
 
     def check_radar(self, degree, map):
         len = 0
         x = int(self.pos[0] + math.cos(math.radians(360 - (self.angle + degree))) * len)
         y = int(self.pos[1] + math.sin(math.radians(360 - (self.angle + degree))) * len)
 
-        if x < screen_width or y < screen_height:
-            while map.get_at((x, y)) == (25, 25, 25, 255) and len < 300:
+        while len < 300 and (x < (screen_width - self.radius) and y < (screen_height - self.radius)) and (x > 0 and y > 0):
+
+            if x >= (screen_width - self.radius) or y >= (screen_height - self.radius):
+                break
+
+            if map.get_at((x, y)) != (25, 25, 25, 255):
+                break
+            else:
                 len = len + 1
                 x = int(self.pos[0] + math.cos(math.radians(360 - (self.angle + degree))) * len)
                 y = int(self.pos[1] + math.sin(math.radians(360 - (self.angle + degree))) * len)
-                if x >= screen_width or y >= screen_height:
-                    break
+
+
+
 
         dist = int(math.sqrt(math.pow(x - self.pos[0], 2) + math.pow(y - self.pos[1], 2)))
         self.radars.append([(x, y), dist])
@@ -75,11 +83,17 @@ class Criatura:
     def draw_radar(self, screen):
         for r in self.radars:
             pos, dist = r
-            pygame.draw.line(screen, (0, 255, 0), self.center, pos, 1)
+            pygame.draw.line(screen, (0, 255, 0), self.pos, pos, 1)
             pygame.draw.circle(screen, (0, 255, 0), pos, 5)
 
-    def draw(self):
-        pygame.display.flip()
+    def draw(self,screen):
+        pygame.draw.circle(screen, (self.color[0], self.color[1], self.color[2]), (self.pos[0], self.pos[1]), self.radius, self.radius)
+
+        generation_font = pygame.font.SysFont("Arial", 10)
+        text = generation_font.render(str(self.id), True, (255, 255, 0))
+        text_rect = text.get_rect()
+        text_rect.center = (self.pos)
+        screen.blit(text, text_rect)
 
     def get_alive(self):
         return self.is_alive
@@ -105,8 +119,11 @@ class Criatura:
 def run_car(genomes, config):
 
     # Init NEAT
-    nets = []
-    cars = []
+    nets1 = []
+    cars1 = []
+
+    nets2 = []
+    cars2 = []
 
     # Init my game
     pygame.init()
@@ -116,11 +133,20 @@ def run_car(genomes, config):
     i = 0
     for id, g in genomes:
         net = neat.nn.FeedForwardNetwork.create(g, config)
-        nets.append(net)
+        nets1.append(net)
         g.fitness = 0
 
         # Init my cars
-        cars.append(Criatura(screen,clock.tick(),i))
+        cars1.append(Criatura(screen,clock.tick(),i,30,255,0,0))
+        i = i + 1
+
+    for id, g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets2.append(net)
+        g.fitness = 0
+
+        # Init my cars
+        cars2.append(Criatura(screen, clock.tick(),i,10,0,0,255))
         i = i + 1
 
 
@@ -139,8 +165,17 @@ def run_car(genomes, config):
 
 
         # Input my data and get result from network
-        for index, car in enumerate(cars):
-            output = nets[index].activate(car.get_data())
+        for index, car in enumerate(cars1):
+            output = nets1[index].activate(car.get_data())
+            i = output.index(max(output))
+            if i == 0:
+                car.angle += 10
+            else:
+                car.angle -= 10
+
+        # Input my data and get result from network
+        for index, car in enumerate(cars2):
+            output = nets2[index].activate(car.get_data())
             i = output.index(max(output))
             if i == 0:
                 car.angle += 10
@@ -148,29 +183,43 @@ def run_car(genomes, config):
                 car.angle -= 10
 
         # Update car and fitness
-        remain_cars = 0
-        cars_aux = np.copy(cars)
-        for i, car in enumerate(cars):
+        remain_cars1 = 0
+        total_cars = cars1 + cars2
+        for i, car in enumerate(cars1):
             if car.get_alive():
-                remain_cars += 1
-                car.update(cars_aux,screen)
+                remain_cars1 += 1
+                car.update(total_cars,screen)
+                genomes[i][1].fitness += car.get_reward()
+
+        # Update car and fitness
+        remain_cars2 = 0
+        for i, car in enumerate(cars2):
+            if car.get_alive():
+                remain_cars2 += 1
+                car.update(total_cars, screen)
                 genomes[i][1].fitness += car.get_reward()
 
         # check
-        if remain_cars == 0:
+        if remain_cars1 == 0 and remain_cars2 == 0:
             break
 
+        screen.fill(bg)
         # Drawing
-        for car in cars:
+        time.sleep(0.01)
+        for car in cars1:
             if car.get_alive():
-                car.draw()
+                car.draw(screen)
+
+        for car in cars2:
+            if car.get_alive():
+                car.draw(screen)
 
         text = generation_font.render("Generation : " + str(generation), True, (255, 255, 0))
         text_rect = text.get_rect()
         text_rect.center = (screen_width/2, 100)
         screen.blit(text, text_rect)
 
-        text = font.render("remain cars : " + str(remain_cars), True, (0, 0, 0))
+        text = font.render("remain cars : " + str(remain_cars1+remain_cars2), True, (0, 0, 0))
         text_rect = text.get_rect()
         text_rect.center = (screen_width/2, 200)
         screen.blit(text, text_rect)
