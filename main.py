9230d67cec03f30eedae7f09ccf67ca5a3287076
alex.aspa <alex.aspa@students.salle.url.edu>
2022-01-04 21:48:15 +0100

    Threads, still errors when eating and other errors to debug

--- main.py
+++ main.py
@@ -6,16 +6,21 @@ import sys
 import random
 import neat
 import time
+import threading
+
 
 screen_width = 1500
 screen_height = 800
 generation = 0
+total_cars = [[],[]]
+
+semaforo = threading.Semaphore(1);
 
 class Criatura:
     def __init__(self,screen,clock,id,rad,r,g,b):
         self.id = id
         self.radius = rad
-        self.pos = [int(random.randint(1, screen_width - self.radius)), int(random.randint(1,screen_height - self.radius))]
+        self.pos = [int(random.randint(self.radius, screen_width - self.radius)), int(random.randint(self.radius,screen_height - self.radius))]
         self.angle = 0
         self.speed = 0
         self.radars = []
@@ -28,7 +33,7 @@ class Criatura:
 
     def update(self,cars,screen):
         # check speed
-        self.speed = 5
+        self.speed = 8
 
         #move
         self.pos[0] += math.cos(math.radians(360 - self.angle)) * self.speed
@@ -116,66 +121,46 @@ class Criatura:
             self.is_alive = False
 
 
-def run_car(genomes, config):
+def run_sp1(genomes, config):
+
+    bg = 25, 25, 25
+    screen.fill(bg)
 
     # Init NEAT
-    nets1 = []
-    cars1 = []
-
-    nets2 = []
-    cars2 = []
-
-    # Init my game
-    pygame.init()
-    screen = pygame.display.set_mode((screen_width, screen_height))
-    clock = pygame.time.Clock()
+    nets = []
+    cars = []
 
     i = 0
     for id, g in genomes:
         net = neat.nn.FeedForwardNetwork.create(g, config)
-        nets1.append(net)
+        nets.append(net)
         g.fitness = 0
 
         # Init my cars
-        cars1.append(Criatura(screen,clock.tick(),i,30,255,0,0))
+        cars.append(Criatura(screen, clock.tick(), i, 40, 255, 0, 0))
         i = i + 1
 
-    for id, g in genomes:
-        net = neat.nn.FeedForwardNetwork.create(g, config)
-        nets2.append(net)
-        g.fitness = 0
-
-        # Init my cars
-        cars2.append(Criatura(screen, clock.tick(),i,10,0,0,255))
-        i = i + 1
-
-
-    generation_font = pygame.font.SysFont("Arial", 70)
-    font = pygame.font.SysFont("Arial", 30)
     bg = 25, 25, 25
     screen.fill(bg)
 
     # Main loop
     global generation
     generation += 1
+
+    total_cars[0] = []
+    for car in cars:
+        total_cars[0].append(car)
+
     while True:
+        semaforo.acquire()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit(0)
 
-
         # Input my data and get result from network
-        for index, car in enumerate(cars1):
-            output = nets1[index].activate(car.get_data())
-            i = output.index(max(output))
-            if i == 0:
-                car.angle += 10
-            else:
-                car.angle -= 10
-
-        # Input my data and get result from network
-        for index, car in enumerate(cars2):
-            output = nets2[index].activate(car.get_data())
+        for index, car in enumerate(cars):
+            output = nets[index].activate(car.get_data())
             i = output.index(max(output))
             if i == 0:
                 car.angle += 10
@@ -183,56 +168,125 @@ def run_car(genomes, config):
                 car.angle -= 10
 
         # Update car and fitness
-        remain_cars1 = 0
-        total_cars = cars1 + cars2
-        for i, car in enumerate(cars1):
-            if car.get_alive():
-                remain_cars1 += 1
-                car.update(total_cars,screen)
-                genomes[i][1].fitness += car.get_reward()
+        remain_cars = 0
+        cars_aux = []
+        i = 0
+        for i in range(len(total_cars)):
+            cars_aux += total_cars[i]
 
-        # Update car and fitness
-        remain_cars2 = 0
-        for i, car in enumerate(cars2):
+        i = 0
+        for i, car in enumerate(cars):
             if car.get_alive():
-                remain_cars2 += 1
-                car.update(total_cars, screen)
+                remain_cars += 1
+                car.update(cars_aux, screen)
                 genomes[i][1].fitness += car.get_reward()
 
         # check
-        if remain_cars1 == 0 and remain_cars2 == 0:
+        if remain_cars == 0:
+            semaforo.release()
             break
 
         screen.fill(bg)
         # Drawing
         time.sleep(0.01)
-        for car in cars1:
+
+        if len(total_cars) > 0:
+            for i in range(len(total_cars)):
+                for car in total_cars[i]:
+                    if car.get_alive():
+                        car.draw(screen)
+                        car.draw_radar(screen)
+
+            pygame.display.flip()
+            clock.tick(0)
+
+        semaforo.release()
+        time.sleep(0.05)
+
+def run_sp2(genomes, config):
+
+    bg = 25, 25, 25
+    screen.fill(bg)
+
+    # Init NEAT
+    nets = []
+    cars = []
+
+    i = 0
+    for id, g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        g.fitness = 0
+
+        # Init my cars
+        cars.append(Criatura(screen, clock.tick(), i, 20, 0, 0, 255))
+        i = i + 1
+
+    bg = 25, 25, 25
+    screen.fill(bg)
+
+    # Main loop
+    global generation
+    generation += 1
+
+    total_cars[1] = []
+    for car in cars:
+        total_cars[1].append(car)
+
+    while True:
+
+        semaforo.acquire()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(0)
+
+        # Input my data and get result from network
+        for index, car in enumerate(cars):
+            output = nets[index].activate(car.get_data())
+            i = output.index(max(output))
+            if i == 0:
+                car.angle += 10
+            else:
+                car.angle -= 10
+
+        # Update car and fitness
+        remain_cars = 0
+        cars_aux = []
+        i = 0
+        for i in range(len(total_cars)):
+            cars_aux += total_cars[i]
+
+        i = 0
+        for i, car in enumerate(cars):
             if car.get_alive():
-                car.draw(screen)
+                remain_cars += 1
+                car.update(cars_aux, screen)
+                genomes[i][1].fitness += car.get_reward()
 
-        for car in cars2:
-            if car.get_alive():
-                car.draw(screen)
+        # check
+        if remain_cars == 0:
+            semaforo.release()
+            break
 
-        text = generation_font.render("Generation : " + str(generation), True, (255, 255, 0))
-        text_rect = text.get_rect()
-        text_rect.center = (screen_width/2, 100)
-        screen.blit(text, text_rect)
+        screen.fill(bg)
+        # Drawing
+        time.sleep(0.01)
 
-        text = font.render("remain cars : " + str(remain_cars1+remain_cars2), True, (0, 0, 0))
-        text_rect = text.get_rect()
-        text_rect.center = (screen_width/2, 200)
-        screen.blit(text, text_rect)
+        if len(total_cars) > 0:
+            for i in range(len(total_cars)):
+                for car in total_cars[i]:
+                    if car.get_alive():
+                        car.draw(screen)
+                        car.draw_radar(screen)
 
-        pygame.display.flip()
-        clock.tick(0)
+            pygame.display.flip()
+            clock.tick(0)
 
-if __name__ == "__main__":
-    # Set configuration file
-    config_path = "./config-feedforward.txt"
-    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
-                                neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+        semaforo.release()
+        time.sleep(0.05)
 
+def run_neat(config,id_specie):
     # Create core evolution algorithm class
     p = neat.Population(config)
 
@@ -242,4 +296,27 @@ if __name__ == "__main__":
     p.add_reporter(stats)
 
     # Run NEAT
-    p.run(run_car, 1000)
+    if (id_specie == 0):
+        p.run(run_sp1, 1000)
+    else:
+        p.run(run_sp2, 1000)
+
+
+if __name__ == "__main__":
+    # Set configuration file
+    config_path = "./config-feedforward.txt"
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+
+    # Init my game
+    pygame.init()
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    clock = pygame.time.Clock()
+
+    threads = list()
+
+    for i in range(2):
+        t = threading.Thread(target=run_neat,args=(config,i))
+        threads.append(t)
+        t.start()
+
